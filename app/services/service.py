0a6d8c343dd9bc0a4bd02fcaef95b78791baa5e9
Brian Peguero <brianpeguero@gmail.com>
2023-01-22 14:06:01 -0500

    reading from env file

--- app/services/service.py
+++ app/services/service.py
@@ -1,8 +1,7 @@
 import os
 import httpx
 
-CAST_SERVICE_HOST_URL = 'http://localhost:8002/api/v1/casts/'
-url = os.environ.get('CAST_SERVICE_HOST_URL') or CAST_SERVICE_HOST_URL
+url = os.environ.get('CAST_SERVICE_HOST_URL')
 
 def is_cast_present(cast_id: int):
     r = httpx.get(f'{url}{cast_id}')
